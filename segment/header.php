3b92aa7049add769ab0dfe7fed0a55f830b2e094
lukim <lukmanhakim8832@gmail.com>
2022-05-21 09:44:24 +0700

    Headre
    memanggil nama dari DB

--- segment/header.php
+++ segment/header.php
@@ -5,7 +5,7 @@
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
     </button>
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.php">
         <div class="">
             <img style="width:40px" src="img/logo1.png">
         </div>
@@ -173,9 +173,11 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown"
                 aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
+                <span
+                    class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo ("Hai, ".$_SESSION['nama']);?></span>
                 <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
             </a>
+
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">
