188eca7ecf5e42d285c5865f7b397de03b20fa24
lukim <lukmanhakim8832@gmail.com>
2022-05-22 12:57:21 +0700

    sidebar
    link tiap halaman

--- segment/sidebar.php
+++ segment/sidebar.php
@@ -20,12 +20,12 @@
            </div>
            <!-- Nav Item - Pages Collapse Menu  -->
        <li <?php if($_GET['m']==2 && $_GET['n']==1){echo "class='nav-item active'";}else{echo "class='nav-item'";} ?>>
-           <a class="nav-link" href="blank.php?m=2&n=1">
+           <a class="nav-link" href="page_stok_masuk.php?m=2&n=1">
                <i class=" fas fa-fw fa-tachometer-alt"></i>
                <span>Stok Masuk</span></a>
        </li>
        <li <?php if($_GET['m']==3 && $_GET['n']==1){echo "class='nav-item active'";}else{echo "class='nav-item'";} ?>>
-           <a class="nav-link" href="blank.php?m=3&n=1">
+           <a class="nav-link" href="page_stok_keluar.php?m=3&n=1">
                <i class="fas fa-fw fa-chart-area"></i>
                <span>Stok Keluar</span></a>
        </li>
@@ -48,11 +48,11 @@
                <div class="bg-white py-2 collapse-inner rounded">
                    <h6 class="collapse-header">Input Data:</h6>
                    <a <?php if($_GET['m']==4 && $_GET['n']==2){echo "class='collapse-item active'";}else{echo "class='collapse-item'";} ?>
-                       href="blank.php?m=4&n=2">Kelompok Tani</a>
+                       href="page_kel_tani.php?m=4&n=2">Kelompok Tani</a>
                    <a <?php if($_GET['m']==4 && $_GET['n']==3){echo "class='collapse-item active'";}else{echo "class='collapse-item'";} ?>
                        href="blank.php?m=4&n=3">Anggota</a>
                    <a <?php if($_GET['m']==4 && $_GET['n']==4){echo "class='collapse-item active'";}else{echo "class='collapse-item'";} ?>
-                       href="blank.php?m=4&n=4">Pupuk</a>
+                       href="page_jenis_pupuk.php?m=4&n=4">Pupuk</a>
                </div>
            </div>
        </li>
@@ -64,17 +64,17 @@
        </div>
        <!-- Nav Item - Pages Collapse Menu -->
        <li <?php if($_GET['m']==5 && $_GET['n']==5){echo "class='nav-item active'";}else{echo "class='nav-item'";} ?>>
-           <a class="nav-link" href="blank.php?m=5&n=5">
+           <a class="nav-link" href="page_administrator.php?m=5&n=5">
                <i class="fas fa-fw fa-tachometer-alt"></i>
                <span>Administrator</span></a>
        </li>
        <li <?php if($_GET['m']==6 && $_GET['n']==6){echo "class='nav-item active'";}else{echo "class='nav-item'";} ?>>
-           <a class="nav-link" href="blank.php?m=6&n=6">
+           <a class="nav-link" href="page_akun.php?m=6&n=6">
                <i class="fas fa-fw fa-chart-area"></i>
                <span>Akun</span></a>
        </li>
        <li <?php if($_GET['m']==7 && $_GET['n']==7){echo "class='nav-item active'";}else{echo "class='nav-item'";} ?>>
-           <a class="nav-link" href="blank.php?m=7&n=7">
+           <a class="nav-link" href="page_lap_masalah.php?m=7&n=7">
                <i class="fas fa-fw fa-chart-area"></i>
                <span>Laporkan Masalah</span></a>
        </li>
