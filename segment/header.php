cc4a0a77adfb58254ed695d8221a6bb904617a37
lukim <lukmanhakim8832@gmail.com>
2022-06-01 07:59:18 +0700

    sidebar

--- segment/header.php
+++ segment/header.php
@@ -1,6 +1,5 @@
 <!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar  fixed-top shadow">
-
     <!-- Sidebar Toggle (Topbar) -->
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
