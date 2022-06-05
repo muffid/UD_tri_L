ee6bc3f3c8f4c2eacdd666882dbefa6d025647d5
Muffid elmahnun <muffid.mahnun@gmail.com>
2022-06-05 20:57:41 +0700

    Auto stash before merge of "main" and "origin/main"

--- page_stok_masuk.php
+++ page_stok_masuk.php
@@ -1,7 +1,15 @@
 <?php
 session_start();
+<<<<<<< Updated upstream
 if (isset($_SESSION['login'])) {
     ?>
+=======
+
+include "connnection.php";
+
+if(isset($_SESSION['login'])){
+?>
+>>>>>>> Stashed changes
 <!DOCTYPE html>
 <html lang="en">
 <!-- =======================================================
@@ -53,6 +61,105 @@ if (isset($_SESSION['login'])) {
                     </div>
 
                     <!-- Konten -->
+
+
+                    <div class="row">
+
+<!-- Area Chart -->
+
+
+
+<div class="col-xl-6 col-lg-6">
+
+            <?php
+                @session_start();
+                if(isset($_SESSION["info"])){
+                ?>
+
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <?php //echo $_SESSION["info"];
+                    echo ($_SESSION["info"]." klik");?>
+                    <button type="button" class="btn btn-light" data-dismiss="alert"
+                        aria-label="Close"> Disini </button> untuk menutup
+                </div>
+
+                <?php
+                    unset($_SESSION["info"]);
+                }
+
+    ?>
+    <div class="card shadow mb-4">
+        <!-- Card Header - Dropdown -->
+        <div
+            class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">Input Kelompok Tani</h6>
+
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+
+            <form method="POST" action="addKelompok.php" enctype="multipart/form-data">
+                
+                <div class="row mb-2">
+                    <!-- Nama Pengirim -->
+                    <label class="col-sm-4 col-form-label"> Pengirim : </label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="pengirim" name="pengirim"
+                            required>
+                        <p style="color:red; font-size:12px;" id="username_hint"></p>
+                    </div>   
+                </div>
+
+                <div class="row mb-4">
+                    <!-- Nama Pengirim -->
+                    <label class="col-sm-4 col-form-label"> Jenis Pupuk : </label>
+                    <div class="col-sm-8">
+                        <select class="custom-select">
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>   
+                </div>
+
+                <div class="row mb-2">
+                    <!-- Nama Pengirim -->
+                    <label class="col-sm-4 col-form-label"> Jumlah (karung) : </label>
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" id="jumlah" name="jumlah"
+                            required>
+                        <p style="color:red; font-size:12px;" id="username_hint"></p>
+                    </div>   
+                </div>
+                
+
+
+                </div>
+            </form>
+            
+
+
+        </div>
+        
+        
+    </div><!--end of div col-->
+
+    <div class="col-xl-6 col-lg-6">
+
+    <div class="card shadow mb-4">
+       
+        <div
+            class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+            <h6 class="m-0 font-weight-bold text-primary">Summary</h6>
+
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+                RINGKASAN
+        </div>
+    </div>
+</div> 
+
 
 
 
