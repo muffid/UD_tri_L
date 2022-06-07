7c6d90eb7335fcfb96ad06e2f71132a6bdafa8cd
Muffid elmahnun <muffid.mahnun@gmail.com>
2022-06-07 20:30:19 +0700

    INSERT STOK MASUK

---
+++ addStokMasuk.php
@@ -0,0 +1,26 @@
+<?php
+include "connection.php";
+
+$namapeng = $_POST['pengirim'];
+$idppk = $_POST['idppk'];
+
+$jumlah = $_POST['jumlah'];
+$total = $_POST['harga'];
+
+date_default_timezone_set('Asia/Jakarta');
+$tanggal= date("D, j M Y ");
+
+
+$sql = "INSERT INTO stok_masuk (ID_SM,Tanggal,ID_PK,Nama_Pengirim,Jumlah_Masuk,Nominal)
+	 VALUES ('','$tanggal','$idppk','$namapeng','$jumlah','$total')";
+     
+if (mysqli_query($conn, $sql)) {
+    session_start();
+    //$_SESSION["info"] = 'Data kelompok tani  berhasil ditambah';
+    //header("Location: page_kel_tani.php?m=4&n=2");
+    echo("berasil jeh");
+    exit();
+} else {
+    echo "Error: " . $sql . "
+" . mysqli_error($conn);
+}
