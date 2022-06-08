6f76d0d5da34b31e306e4664c7706b5fd6e961b5
Muffid elmahnun <muffid.mahnun@gmail.com>
2022-06-08 17:04:21 +0700

    Edit Table Stok Masuk

--- page_stok_masuk.php
+++ page_stok_masuk.php
@@ -36,6 +36,7 @@ if(isset($_SESSION['login'])){
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
     <link href="img/logo1.png" rel="icon">
+    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 </head>
 
 <body id="page-top">
@@ -380,6 +381,14 @@ $no = 1;
 
     <!-- Custom scripts for all pages-->
     <script src="js/sb-admin-2.min.js"></script>
+
+    
+    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/datatables-demo.js"></script>
+
     <script>
     function countPerKarung(sel) {
 
