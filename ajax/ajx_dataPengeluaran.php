6d44e1901593d10a121cb48d79a03e60e91b5790
lukim <lukmanhakim8832@gmail.com>
2022-06-20 16:40:11 +0700

    Ajax

---
+++ ajax/ajx_dataPengeluaran.php
@@ -0,0 +1,66 @@
+<?php
+include "../connection.php";
+include '../functions.php';
+$key = $_POST['key'];
+$no = 1;
+$output = "";
+$total = 0;
+//2022-02
+// algoritma ngganti bulan disini
+$tahun = substr($key, 0, 4);
+$bln = substr($key, 5, 7);
+switch ($bln) {
+    case '01':
+        $bln = 'Jan';
+        break;
+    case '02':
+        $bln = 'Feb';
+        break;
+    case '03':
+        $bln = 'Mar';
+        break;
+    case '04':
+        $bln = 'Apr';
+        break;
+    case '05':
+        $bln = 'May';
+        break;
+    case '06':
+        $bln = 'Jun';
+        break;
+    case '07':
+        $bln = 'Jul';
+        break;
+    case '08':
+        $bln = 'Aug';
+        break;
+    case '09':
+        $bln = 'Sep';
+        break;
+    case '10':
+        $bln = 'Oct';
+        break;
+    case '11':
+        $bln = 'Nov';
+        break;
+    case '12':
+        $bln = 'Dec';
+        break;
+}
+$blntahun = $bln . " " . $tahun;
+$getData = mysqli_query($conn, "SELECT *FROM stok_masuk
+                        INNER JOIN data_pupuk ON stok_masuk.ID_PK = data_pupuk.ID_PK WHERE Tanggal LIKE '%" . $blntahun . "%' ORDER BY ID_SM DESC");
+foreach ($getData as $gd) {
+    $output .=
+    '<tr>
+                            <td>' . $no . '</td>
+                            <td>' . $gd['Tanggal'] . '</td>
+                            <td>' . "Pupuk " . $gd['Jenis_Pupuk'] . '</td>
+                            <td>' . $gd['Jumlah_Masuk'] . " Karung" . '</td>
+                            <td>' . rp($gd['Nominal']) . '</td>
+                          </tr>';
+    $total = $total + (int) $gd['Nominal'];
+    $no++;
+}
+$output .= '<tr><td colspan="4" class="text-center">TOTAL</td><td>' . $total . '</td></tr>';
+echo ($output);
