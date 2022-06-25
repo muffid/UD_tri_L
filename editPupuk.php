1fbdda1f07d51296a1e33f96c63968541239808a
lukim <lukmanhakim8832@gmail.com>
2022-06-25 13:43:58 +0700

    ok editpupuk.php

--- editPupuk.php
+++ editPupuk.php
@@ -1,21 +1,18 @@
-<?php 
+<?php
 include "connection.php";
 
-$editnamapupuk=$_POST['editnamapupuk'];
-$edithargapupuk=$_POST['edithargapupuk'];
-$editIDpupuk=$_GET['id'];
+$editnamapupuk = $_POST['editnamapupuk'];
+$edithargapupuk = htmlspecialchars(str_replace(".", "", $_POST['edithargapupuk']));
+$editIDpupuk = $_GET['id'];
 
 //echo($editnamapupuk.$edithargapupuk.$editIDpupuk);
-$sql = "UPDATE data_pupuk SET Jenis_Pupuk='".$editnamapupuk."',Harga='".$edithargapupuk."' WHERE ID_PK=".$editIDpupuk;
+$sql = "UPDATE data_pupuk SET Jenis_Pupuk='" . $editnamapupuk . "',Harga='" . $edithargapupuk . "' WHERE ID_PK=" . $editIDpupuk;
 if (mysqli_query($conn, $sql)) {
     session_start();
     $_SESSION["info"] = 'Data pupuk berhasil diubah';
-   header("Location: page_jenis_pupuk.php?m=4&n=4");
-   exit();
+    header("Location: page_jenis_pupuk.php?m=4&n=4");
+    exit();
 } else {
-   echo "Error: " . $sql . "
+    echo "Error: " . $sql . "
 " . mysqli_error($conn);
 }
-
-
-?>
