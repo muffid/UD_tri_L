27572289e14df8d44989a20be9be71499f8220f3
Muffid elmahnun <muffid.mahnun@gmail.com>
2022-06-25 15:16:53 +0700

    FIXED BUG editpupuk

--- editPupuk.php
+++ editPupuk.php
@@ -1,12 +1,16 @@
 <?php 
 include "connection.php";
 
-$editnamapupuk=$_POST['editnamapupuk'];
-$edithargapupuk=$_POST['edithargapupuk'];
 $editIDpupuk=$_GET['id'];
 
+$editnamapupuk=$_POST['editnamapupuk'.$editIDpupuk];
+$edithargapupuk=$_POST['edithargapupuk'.$editIDpupuk];
+$hargaFix=str_replace(".","",str_replace("Rp. ","",$edithargapupuk));
+
+
+
 //echo($editnamapupuk.$edithargapupuk.$editIDpupuk);
-$sql = "UPDATE data_pupuk SET Jenis_Pupuk='".$editnamapupuk."',Harga='".$edithargapupuk."' WHERE ID_PK=".$editIDpupuk;
+$sql = "UPDATE data_pupuk SET Jenis_Pupuk='".$editnamapupuk."',Harga='".$hargaFix."' WHERE ID_PK=".$editIDpupuk;
 if (mysqli_query($conn, $sql)) {
     session_start();
     $_SESSION["info"] = 'Data pupuk berhasil diubah';
@@ -17,5 +21,19 @@ if (mysqli_query($conn, $sql)) {
 " . mysqli_error($conn);
 }
 
+function rp($angka)
+{
+
+    $hasil_rupiah = "Rp. " . number_format($angka, 0, ',', '.');
+    return $hasil_rupiah;
+
+}
+function rpnull($angka1)
+{
+
+    $hasil_rupiah1 = number_format($angka1, 0, ',', '.');
+    return $hasil_rupiah1;
+
+}
 
 ?>
