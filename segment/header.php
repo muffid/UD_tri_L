665012210b0861351c69d09e80590bf20b9c3c49
lukim <lukmanhakim8832@gmail.com>
2022-06-28 13:14:16 +0700

    oko

--- segment/header.php
+++ segment/header.php
@@ -98,7 +98,7 @@
         <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
           <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-          Logout
+          Keluar
         </a>
       </div>
     </li>
@@ -113,15 +113,15 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Keluar Program ?</h5>
         <button class="close" type="button" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">×</span>
         </button>
       </div>
-      <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+      <div class="modal-body">Jika anda yakin megakhiri program klik "Keluar"</div>
       <div class="modal-footer">
-        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-        <a class="btn btn-primary" href="logout.php">Logout</a>
+        <button class="btn btn-secondary" type="button" data-dismiss="modal">Tidak</button>
+        <a class="btn btn-primary" href="logout.php">Keluar</a>
       </div>
     </div>
   </div>
