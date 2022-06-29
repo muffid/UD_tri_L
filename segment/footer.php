87be76f20ebdac440431f4a1811b4e660093cdb7
lukim <lukmanhakim8832@gmail.com>
2022-06-29 10:02:47 +0700

    Footer

--- segment/footer.php
+++ segment/footer.php
@@ -8,19 +8,19 @@
   ======================================================== -->
 <!-- Footer -->
 <footer class="sticky-footer bg-white">
-    <div class="container my-auto">
-        <div class="copyright text-center my-auto">
-            <span>Copyright &copy; <strong>UD. TRI L</strong>. All Rights Reserved</span>
-        </div>
-        <div class="copyright text-center mt-1">
-
-            <span>Designed & Developed by: <a href="https://anemos.id/">Anemos.id</a></span>
-
-        </div>
+  <div class="container my-auto">
+    <div class="copyright text-center my-auto">
+      <span>Copyright &copy; 2022<strong> UD. TRI L</strong>. All Rights Reserved</span>
     </div>
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
+    <div class="copyright text-center mt-1">
+
+      <span>Designed & Developed by: <a href="https://anemos.id/">Anemos.id</a></span>
+
+    </div>
+  </div>
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
 </footer>
 <!-- End of Footer -->
