1c70a4e75badd23bd2919bdfb2d44b3ab6c917c3
lukim <lukmanhakim8832@gmail.com>
2022-06-29 16:38:57 +0700

    Edit

--- blank.php
+++ blank.php
@@ -13,23 +13,23 @@ if (isset($_SESSION['login'])) {
   ======================================================== -->
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
 
-    <title>UD. Tri L | Blank</title>
+  <title>UD. Tri L | Blank</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <?php
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <?php
 // header
     include "segment/header.php";
     //    end Header
@@ -39,53 +39,54 @@ if (isset($_SESSION['login'])) {
 
 <body id="page-top">
 
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-        <!-- include sidebar -->
-        <?php include "segment/sidebar.php";?>
-        <!-- end include sidebar -->
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+    <!-- include sidebar -->
+    <?php include "segment/sidebar.php";?>
+    <!-- end include sidebar -->
 
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
 
-            <!-- Main Content -->
-            <div id="content">
+      <!-- Main Content -->
+      <div id="content">
 
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
 
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mt-5">
-                        <h1 class="h3 mt-5 text-gray-800">Blank Page</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mt-4"><i
-                                class="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
-                    </div>
-
-
-                </div>
-
-            </div>
-            <!-- End of Main Content -->
-
-            <!-- footer -->
-            <?php include "segment/footer.php";?>
-            <!-- end footer -->
-
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mt-5">
+            <h1 class="h3 mt-5 text-gray-800">Blank Page</h1>
+            <a href="index.php?m=1&n=1" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mt-4"><i
+                class="far fa-arrow-alt-circle-left  text-white-50"></i> Kembali Ke Halaman Dashboard</a>
+          </div>
+          <center class=" mt-5">
+            <h1 class=" mt-5"> Fitur Ini Belum Terupgrade </h1>
+          </center>
         </div>
-        <!-- End of Content Wrapper -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- footer -->
+      <?php include "segment/footer.php";?>
+      <!-- end footer -->
 
     </div>
-    <!-- End of Page Wrapper -->
+    <!-- End of Content Wrapper -->
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  </div>
+  <!-- End of Page Wrapper -->
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
