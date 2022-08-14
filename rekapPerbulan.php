f9b4b20587d25bc44237e7c65b6b18e4b2e9a19f
lukim <lukmanhakim8832@gmail.com>
2022-08-14 22:00:35 +0700

    blm fix

--- rekapPerbulan.php
+++ rekapPerbulan.php
@@ -13,81 +13,123 @@ if (isset($_SESSION['login'])) {
   ======================================================== -->
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <title>UD. Tri L | Rekap Perbulan</title>
+    <title>UD. Tri L | Rekap Perbulan</title>
 
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link
-    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-    rel="stylesheet">
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
-  <link href="img/logo1.png" rel="icon">
+    <link href="img/logo1.png" rel="icon">
 </head>
 
 <body id="page-top">
 
-  <!-- Page Wrapper -->
-  <div id="wrapper">
-    <!-- include sidebar -->
-    <?php include "segment/sidebar.php";?>
-    <!-- end include sidebar -->
+    <!-- Page Wrapper -->
+    <div id="wrapper">
+        <!-- include sidebar -->
+        <?php include "segment/sidebar.php";?>
+        <!-- end include sidebar -->
 
-    <!-- Content Wrapper -->
-    <div id="content-wrapper" class="d-flex flex-column">
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
-        <?php
+            <!-- Main Content -->
+            <div id="content">
+                <?php
 // header
     include "segment/header.php";
     //    end Header
     ?>
-        <!-- Begin Page Content -->
-        <div class="container-fluid">
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
 
-          <!-- Page Heading -->
-          <hr class="sidebar-divider my-0 mt-5 mb-5">
-          <div class="d-sm-flex align-items-center justify-content-between ">
-            <h1 class="h3 mb-3 text-gray-800">Rekap Perbulan</h1>
-            <a href="index.php?m=1&n=1" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mb-4"><i
-                class=" far fa-arrow-alt-circle-left text-white-50"></i> Kembali Ke Halaman Dashboard</a>
-          </div>
-          <center class=" mt-5">
-            <h1 class=" mt-5"> Fitur Ini Belum Terupgrade </h1>
-          </center>
+                    <!-- Page Heading -->
+                    <hr class="sidebar-divider my-0 mt-5 mb-5">
+                    <div class="d-sm-flex align-items-center justify-content-between ">
+                        <h1 class="h3 mb-3 text-gray-800">Rekap Perbulan</h1>
+                        <a href="index.php?m=1&n=1"
+                            class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm mb-4"><i
+                                class=" far fa-arrow-alt-circle-left text-white-50"></i> Kembali Ke Halaman
+                            Dashboard</a>
+                    </div>
+
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3">
+                            <h6 class="m-0 font-weight-bold text-primary">Rekap Pupuk</h6>
+                        </div>
+                        <div class="card-body">
+                            <p>Use Font Awesome Icons (included with this theme package) along with the circle
+                                buttons as shown in the examples below!</p>
+
+                            <table class="table table-bordered">
+                                <thead class=" thead-dark">
+                                    <tr>
+                                        <th scope="col">No</th>
+                                        <th scope="col">Nama Pupuk</th>
+                                        <th colspan="2">Juli</th>
+                                    </tr>
+                                    <tr>
+                                        <th scope="col">No</th>
+                                        <th scope="col">Nama Pupuk</th>
+                                        <th colspan="2">Juli</th>
+                                    </tr>
+
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Urea</td>
+                                        <td>200</td>
+
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Phoska</td>
+                                        <td>200</td>
+
+                                    </tr>
+                                </tbody>
+
+                            </table>
+
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+            <!-- End of Main Content -->
+
+            <!-- footer -->
+            <?php include "segment/footer.php";?>
+            <!-- end footer -->
+
         </div>
-
-      </div>
-      <!-- End of Main Content -->
-
-      <!-- footer -->
-      <?php include "segment/footer.php";?>
-      <!-- end footer -->
+        <!-- End of Content Wrapper -->
 
     </div>
-    <!-- End of Content Wrapper -->
+    <!-- End of Page Wrapper -->
 
-  </div>
-  <!-- End of Page Wrapper -->
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="js/sb-admin-2.min.js"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
